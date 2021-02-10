b42382ec2eb3e4e6cf37072af0ab5adf93b3d12b
Josue Santos <josuesantosfls@gmail.com>
2021-02-10 12:30:02 -0600

    Almost Done

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,5 @@
 
 function menu(){
-    document.getElementById("main_page").style.webkitFilter = "blur(1.5px)";
+    document.getElementById("main_page").style.webkitFilter = "blur(3px)";
     document.getElementById("menu_div").style.display="block";
 }
