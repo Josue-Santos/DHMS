18ebe2c8b5d7612ec7c21156bbe577e98b506d6a
Josue Santos <josuesantosfls@gmail.com>
2021-02-15 16:24:13 -0600

    Sliding Menu Feature

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,35 @@
-
 function menu(){
-    document.getElementById("main_page").style.webkitFilter = "blur(3px)";
-    document.getElementById("menu_div").style.display="block";
+    if(screen.width <= 812){
+        document.getElementById("main_page").style.webkitFilter = "blur(3px)";
+        document.getElementById("menu_div").style.width="150px";
+        document.getElementById("closebtn").style.fontSize="50px";
+        document.getElementById("menu_pic").style.width="300px";
+        links = document.getElementsByClassName('menu_options');
+        for(var i = 0; i<5;i++){
+            links[i].style.fontSize="25px";
+        }
+        
+    }
+    else{
+        document.getElementById("main_page").style.webkitFilter = "blur(3px)";
+        document.getElementById("menu_div").style.width="400px";
+        document.getElementById("closebtn").style.fontSize="50px";
+        document.getElementById("menu_pic").style.width="450px";
+        links = document.getElementsByClassName('menu_options');
+        for(var i = 0; i<5;i++){
+            links[i].style.fontSize="50px";
+        }
+    }      
+
+}
+function closeMenu(){
+    document.getElementById("main_page").style.webkitFilter="blur(0px)";
+    document.getElementById("menu_div").style.width="0px";
+    document.getElementById("closebtn").style.fontSize="0px";
+    document.getElementById("menu_pic").style.width="0px";
+    links = document.getElementsByClassName('menu_options');
+    for(var i = 0; i<5;i++){
+        links[i].style.fontSize="0px";
+    }
+
 }
