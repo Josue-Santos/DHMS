40a740629bf7b1ed47baf3cf99d16284791d81df
Josue Santos <josuesantosfls@gmail.com>
2021-02-18 16:13:45 -0600

    Fixing some mobile friendly issues, adding media queries to index,and about

--- js/menu.js
+++ js/menu.js
@@ -2,6 +2,7 @@ function menu(){
     if(screen.width <= 812){
         document.getElementById("main_page").style.webkitFilter = "blur(3px)";
         document.getElementById("menu_div").style.width="150px";
+        document.getElementById("menu_div").style.borderRight= "5px solid white";
         document.getElementById("closebtn").style.fontSize="50px";
         document.getElementById("menu_pic").style.width="300px";
         links = document.getElementsByClassName('menu_options');
@@ -13,6 +14,8 @@ function menu(){
     else{
         document.getElementById("main_page").style.webkitFilter = "blur(3px)";
         document.getElementById("menu_div").style.width="400px";
+        document.getElementById("menu_div").style.borderRight= "5px solid white";
+        document.getElementById("closebtn").style.fontSize="50px";
         document.getElementById("closebtn").style.fontSize="50px";
         document.getElementById("menu_pic").style.width="450px";
         links = document.getElementsByClassName('menu_options');
